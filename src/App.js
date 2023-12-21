cd679de30fe4ddb1c614742b6e448f500803982a
Alexkor <Alexkor1303@yandex.ru>
2023-12-21 08:43:05 +0300

    categories useState added

--- src/App.js
+++ src/App.js
@@ -3,21 +3,22 @@ import Header from "./components/Header";
 import Sort from "./components/Sort";
 import Categories from "./components/Categories";
 import PizzaBlock from "./components/Pizzablock";
+import React from "react";
+
 function App() {
   return (
     <div className="App">
-      <div class="wrapper">
+      <div className="wrapper">
         <Header />
-        <div class="content">
-          <div class="container">
-            <div class="content__top">
+        <div className="content">
+          <div className="container">
+            <div className="content__top">
               <Categories />
               <Sort />
             </div>
-            <h2 class="content__title">Все пиццы</h2>
-            <div class="content__items">
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
               <PizzaBlock title="Мексиканская" price="777" />
-              <PizzaBlock />
               <PizzaBlock />
             </div>
           </div>
